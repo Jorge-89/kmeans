02d208e7f55a2eec03a95ae4454be7072312a2b1
Jorge Ruiz <79252196+Jorge-89@users.noreply.github.com>
2022-07-31 12:52:09 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI, File, Form, UploadFile
+import uvicorn
+from pydantic import BaseModel
+import pickle
+import numpy as np
+import pandas as pd
+from io import StringIO
+
+app = FastAPI()
+
+class Kmeans(BaseModel):
+    edad: float 
+    salario: float 
+    score: float 
+    
+
+@app.post('/predict')
+async def clasificadores(variables_entrada: Kmeans):
+    data = variables_entrada.dict()
+    loaded_model = pickle.load(open('model.pkl', 'rb'))
+    data_in = [[data['edad'], data['salario'], data['score']]]
+    prediction = loaded_model.predict(data_in)
+    
+   
+            
+    return int(prediction)
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=4000, debug=True)
